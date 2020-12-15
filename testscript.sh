82b724dec1adef0ed2df45720e3a5e21cc9a9701
Yantrapragada, Arun (ay296m) <ay296m@us.att.com>
2020-12-15 12:15:31 -0500

    checking if commit will trigger the build

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,8 @@ echo "simple test script to check if jenkins is working as intended"
 rpm -qa | grep 'jenkins'
 echo "------"
 rpm -qa | grep 'ssh'
+echo "adding one more command"
+ls -la /datadrive/
+
 
 exit 0
