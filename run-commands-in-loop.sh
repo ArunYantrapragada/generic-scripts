a8701dece099098f79b1892d3d3b5a6535e7e141
Yantrapragada, Arun (ay296m) <ay296m@us.att.com>
2021-02-03 23:38:50 -0500

    modified script

--- run-commands-in-loop.sh
+++ run-commands-in-loop.sh
@@ -1,28 +1,24 @@
-#!/bin/ksh
-
+#!/bin/bash
 #Script to run commands in loop for multiple servers by reading ip's from file as input. 
- INFILE=$1
+INFILE=$1
  
- if [[ ! -s $INFILE ]]
- then
+if [[ ! -s $INFILE ]]
+then
    echo "ERROR : INFILE ($INFILE) does NOT exist"
    exit 99
- else
+else
    echo "INFILE ($INFILE) does exist"
- fi
+fi
  
- for ipaddr in `cat $INFILE`
- do
- echo "running for $ipaddr"
+for ipaddr in `cat $INFILE`
+do
+echo "running for $ipaddr"
  
- #Key pair to run against ec2-instances
- ssh -i <ssh-key> user@$ipaddr  << EOF
- ls -a
- sudo -i
- 
- #command/script to run at this section...
- ls -al 
- ##
- 
- EOF
- done
+#Key pair to run against ec2-instances
+ssh -i <ssh-key> user@$ipaddr  << EOF
+#command/script to run at this section...
+ls -al 
+##
+
+EOF
+done
